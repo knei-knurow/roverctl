0cd0a7b9f0282ff4ccba82004b315abc157064d2
Bartłomiej Pacia <bartlomiej.pacia@euvic.pl>
2021-06-18 20:44:38 +0200

    shit

--- ui/main.py
+++ ui/main.py
@@ -4,7 +4,10 @@ import subprocess
 window = tk.Tk()
 
 speedVar = tk.IntVar()
+speedVar.initialize(120)
+
 degreesVar = tk.IntVar()
+degreesVar.initialize(90)
 
 # handlers
 def handle_go_forward(event: tk.Event):
@@ -36,7 +39,7 @@ def handle_go_stop(event: tk.Event):
 
 
 def handle_go_backward(event: tk.Event):
-    print(f"handle_go")
+    print(f"handle_go_backward")
     proc = subprocess.run(
         [
             "./roverctl",
@@ -73,10 +76,17 @@ label.pack(padx=5, pady=5)
 label = tk.Label(master=frame0, text=f"WARNING: \nalways stop when changing direction!")
 label.pack(padx=5, pady=5)
 
-# window.rowconfigure(index=1, weight=1, minsize=50)
-# window.columnconfigure(index=0, weight=1, minsize=75)
+labelSpeed = tk.Label(master=frame0, text=f"speed")
+labelSpeed.pack(padx=5, pady=5)
 
-# entrySpeed = tk.Entry(master=frame1, textvariable=speedVar, fg="red", width=10)
+entryDegrees = tk.Entry(
+    master=frame0,
+    textvariable=speedVar,
+    fg="black",
+    width=10,
+    font=("calibre", 10, "normal"),
+)
+entryDegrees.pack()
 
 btnGoForward = tk.Button(master=frame0, text="go forward")
 btnGoForward.pack(padx=5, pady=5)
@@ -92,12 +102,24 @@ btnGoBackward = tk.Button(master=frame0, text="go backward")
 btnGoBackward.pack(padx=5, pady=5)
 btnGoBackward.bind("<Button-1>", handle_go_backward)
 
+label = tk.Label(master=frame0, text=f"TURN commands")
+label.pack(padx=5, pady=5)
+
+degreesSpeed = tk.Label(master=frame0, text=f"degrees")
+degreesSpeed.pack(padx=5, pady=5)
+
+entryDegrees = tk.Entry(
+    master=frame0,
+    textvariable=degreesVar,
+    fg="black",
+    width=10,
+    font=("calibre", 10, "normal"),
+)
+entryDegrees.pack()
+
 btnTurn = tk.Button(master=frame0, text="turn")
 btnTurn.pack(padx=5, pady=5)
 btnTurn.bind("<Button-1>", handle_turn)
-
-# frame0 = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
-# frame0.grid(row=0, column=1, padx=5, pady=5)
 
 
 window.mainloop()
