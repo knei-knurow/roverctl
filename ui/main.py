754506e0cc57bff7efda7bb1cff14c1598f2da7d
Bartłomiej Pacia <bartlomiej.pacia@euvic.pl>
2021-06-18 19:45:55 +0200

    add all handlers for GO command

--- ui/main.py
+++ ui/main.py
@@ -3,25 +3,62 @@ import subprocess
 
 window = tk.Tk()
 
-speed = 120
-degrees = 90
+speedVar = tk.IntVar()
+degreesVar = tk.IntVar()
 
 # handlers
-def handle_go(event: tk.Event):
+def handle_go_forward(event: tk.Event):
     print(f"handle_go")
-    proc = subprocess.run(["./roverctl", "go", "--speed", f"{speed}", "forward"])
+    proc = subprocess.run(
+        [
+            "./roverctl",
+            "go",
+            "--speed",
+            f"{speedVar.get()}",
+            "forward",
+        ]
+    )
     print(f"return code: {proc.returncode}")
 
 
 def handle_go_stop(event: tk.Event):
     print(f"handle_go_stop")
-    proc = subprocess.run(["./roverctl", "go", "--speed", "255", "stop"])
+    proc = subprocess.run(
+        [
+            "./roverctl",
+            "go",
+            "--speed",
+            "255",
+            "stop",
+        ]
+    )
+    print(f"return code: {proc.returncode}")
+
+
+def handle_go_backward(event: tk.Event):
+    print(f"handle_go")
+    proc = subprocess.run(
+        [
+            "./roverctl",
+            "go",
+            "--speed",
+            f"{speedVar.get()}",
+            "backward",
+        ]
+    )
     print(f"return code: {proc.returncode}")
 
 
 def handle_turn(degrees: int):
     print(f"handle_go_turn")
-    proc = subprocess.run(["./roverctl", "turn", "--degrees", f"{degrees}"])
+    proc = subprocess.run(
+        [
+            "./roverctl",
+            "turn",
+            "--degrees",
+            f"{degreesVar.get()}",
+        ]
+    )
     print(f"return code: {proc.returncode}")
 
 
@@ -30,6 +67,8 @@ window.columnconfigure(index=0, weight=1, minsize=75)
 
 frame1 = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
 frame1.grid(row=0, column=0, padx=5, pady=5)
+
+entrySpeed = tk.Entry(master=frame1, textvariable=speedVar)
 
 label = tk.Label(master=frame1, text=f"GO commands")
 label.pack(padx=5, pady=5)
@@ -44,16 +83,17 @@ frame1.grid(row=1, column=0, padx=5, pady=5)
 
 btnGoForward = tk.Button(master=frame1, text="go forward")
 btnGoForward.pack(padx=5, pady=5)
-btnGoForward.bind("<Button-1>", handle_go)
+btnGoForward.bind("<Button-1>", handle_go_forward)
 
 
 btnGoStop = tk.Button(master=frame1, text="go stop")
 btnGoStop.pack(padx=5, pady=5)
-btnGoStop.bind("<Button-2>", handle_go_stop)
+btnGoStop.bind("<Button-1>", handle_go_stop)
 
 
 btnGoBackward = tk.Button(master=frame1, text="go backward")
 btnGoBackward.pack(padx=5, pady=5)
+btnGoBackward.bind("<Button-1>", handle_go_backward)
 
 
 window.mainloop()
